6bf7bcaf0ac1c372b66f101858d76e68f00e9629
alexvallcorba <alexgvallcorba@gmail.com>
2021-10-18 17:38:57 -0400

    changed home page style

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className='home'>
-      <div className="box" >
-      <h1 className="name" >alex vallcorba</h1>
-      <h3 className="title" >
-      full-stack web developer
-        </h3>
-        </div>
+    <div className="home">
+      <div className="box">
+        <h1 className="name">alex vallcorba</h1>
+        <h3 className="title">full-stack web developer</h3>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
