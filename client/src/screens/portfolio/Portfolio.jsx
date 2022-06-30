03a97d49c3213accb0dd817e71208a2937b363d3
alexvallcorba <alexgvallcorba@gmail.com>
2022-06-30 13:11:46 -0400

    Edits

--- client/src/screens/portfolio/Portfolio.jsx
+++ client/src/screens/portfolio/Portfolio.jsx
@@ -5,15 +5,39 @@ import { Link } from "react-router-dom";
 export default function Portfolio() {
   return (
 <div>
-      <h1 className="portfolio-title">PROJECTS</h1>
+      <h1 className="portfolio-title">WEB DEVELOPMENT</h1>
 
       
-    <div className="portfolio-container">
+      <div className="portfolio-container">
+      <div className="project-description">
+        <h3 className="project-name">ORWEL THE CHICKEN AVENGER</h3>
+          <p className="Info-project">
+            App for promotion of  the graphic nobel with the same name to comic book and culinary fans. The users can navigate the app to meet the characters of the comic and learn about their characteristic and they also can create and delete sggestions for new characters. Comes with recipes that the user can download. Has a 30sec comercial of the comic made with Adobe AfterEffects. Utilized: React, CSS, Airtable.
+          </p>
+          <h4 className="click">Click on image to go to website</h4>
+      </div>
+      <div className="portfolio">
+        <div className="landing-page-image">
+          <a className="anchor" target="_blank" href="https://orwel-the-chicken-avenger.netlify.app/">
+            <img className="image-app" src="https://res.cloudinary.com/dhkeoqhmp/image/upload/v1634575000/PROJECT-4.%20KAIMANA%20SURFBOARDS%20APP/orwel-app_nb8u5f.png" alt="Orwel the Chicken Avenger" />
+          </a>
+        </div>
+          <div className="info">
+         <p className="link-github-code" >View project's code</p>
+        <a className="anchor1" target="_blank" href="https://github.com/alexvallcorba/orwel-the-chicken-avenger">
+            <img className="github-logo" src="https://res.cloudinary.com/dhkeoqhmp/image/upload/e_sepia:80/v1634583817/PROJECT-4.%20KAIMANA%20SURFBOARDS%20APP/github-logo-blue_bmcetd.png" alt="Github logo" />
+            
+          </a>
+          
+        </div>
+        </div>
         <div className="project-description">
           <h3 className="project-name">KAIMANA SURFBOARDS</h3>
           <p className="Info-project">
           Web application that allows the user to shop for surfboards and create their custom surfboards. Full CRUD and authentication. Utilized: React, Rubi on Rails, CSS. (WORK IN PROGRESS)
           </p>
+          <h4 className="click">Click on image to go to website</h4>
+
       </div>
       <div className="portfolio">
         <div className="landing-page-image">
@@ -34,6 +58,8 @@ export default function Portfolio() {
           <p className="Info-project">
             Web application built in a team of fellow junior engineers that allows the user to easily navigate  coffee blends and merchandise. Full CRUD and authentication. Utilized: React, Express, Mongo DB, Mongoose, Bootstarp, CSS.
           </p>
+          <h4 className="click">Click on image to go to website</h4>
+
       </div>
       <div className="portfolio">
         <div className="landing-page-image">
@@ -49,32 +75,14 @@ export default function Portfolio() {
           
         </div>
         </div>
-        <div className="project-description">
-        <h3 className="project-name">ORWEL THE CHICKEN AVENGER</h3>
-          <p className="Info-project">
-            App for promotion of  the graphic nobel with the same name to comic book and culinary fans. The users can navigate the app to meet the characters of the comic and learn about their characteristic and they also can create and delete sggestions for new characters. Comes with recipes that the user can download. Has a 30sec comercial of the comic made with Adobe AfterEffects. Utilized: React, CSS, Airtable.
-          </p>
-      </div>
-      <div className="portfolio">
-        <div className="landing-page-image">
-          <a className="anchor" target="_blank" href="https://orwel-the-chicken-avenger.netlify.app/">
-            <img className="image-app" src="https://res.cloudinary.com/dhkeoqhmp/image/upload/v1634575000/PROJECT-4.%20KAIMANA%20SURFBOARDS%20APP/orwel-app_nb8u5f.png" alt="Orwel the Chicken Avenger" />
-          </a>
-        </div>
-          <div className="info">
-         <p className="link-github-code" >View project's code</p>
-        <a className="anchor1" target="_blank" href="https://github.com/alexvallcorba/orwel-the-chicken-avenger">
-            <img className="github-logo" src="https://res.cloudinary.com/dhkeoqhmp/image/upload/e_sepia:80/v1634583817/PROJECT-4.%20KAIMANA%20SURFBOARDS%20APP/github-logo-blue_bmcetd.png" alt="Github logo" />
-            
-          </a>
-          
-        </div>
-        </div>
+       
         <div className="project-description">
           <h3 className="project-name">COOL & WONDERFUL GUIDE 2 THE WORLD</h3>
           <p className="Info-project">
             Travel guide that allows users to search anyshops, airports, hotels, restaurants, tourist locations with their address, map location and price rating all over the world. Utilized: Javascript, CSS, HTML, Yelp API.
           </p>
+          <h4 className="click">Click on image to go to website</h4>
+
       </div>
       <div className="portfolio">
         <div className="landing-page-image">
